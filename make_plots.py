3003bb856a543daff1a612c96897a87f6fb75826
Keesjan de Vries <keesjan777@gmail.com>
2013-05-13 13:33:15 +0100

    start adding commandline arguments

--- make_plots.py
+++ make_plots.py
@@ -3,3 +3,14 @@
 import user.axes
 import user.spaces
 import user.files
+import py_modules.oldarrayindices
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('filename', help='define input root file')
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args=parse_args()
