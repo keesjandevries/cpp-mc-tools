f1fe60b9c72ee1bb05ba8c73900dbd8c261e36ac
Keesjan de Vries <keesjan777@gmail.com>
2013-05-14 13:29:13 +0100

    add some spaces

--- user/spaces.py
+++ user/spaces.py
@@ -1,10 +1,11 @@
 def get_spaces():
     return [
-            {   'axes':['m0','m12'],
-                'zaxes':['mh','BsmmRatio'],
-                },
+            {   'axes':['m0','m12'], 'zaxes':['mh','BsmmRatio'], },
+            {   'axes':['MA','tanb'],'zaxes':'BsmmRatio'},
+            {   'axes':['m0','tanb'],'zaxes':'BsmmRatio'},
+            {   'axes':['A0','tanb'],'zaxes':'BsmmRatio'},
+            {   'axes':['tanb','m12'],'zaxes':'BsmmRatio'},
             {   'axes':'mh' }, 
             {   'axes':'BsmmRatio', 'zaxes':'BsmmRatio' }, 
             {   'axes':'m0'},
-            {   'axes':['MA','tanb'],'zaxes':'BsmmRatio'},
         ]
