29759fbe919d6b55dc289aeb987aefd032a5c45f
Keesjan de Vries <keesjan777@gmail.com>
2013-05-15 10:31:20 +0100

    add file

--- user/files.py
+++ user/files.py
@@ -6,4 +6,7 @@ def get_files():
             '/vols/cms04/kjd110/nuhm1_mc8_boxes_mh2/nuhm1-boxesmc8.root':{
                 'observable_ids':{ 'mc_old' :{ 'prediction_index':12, 'spectrum_index':76  }}, 
                 },
+            '/vols/cms04/kjd110/nuhm1_mc8_boxes_mh2_fix//nuhm1-boxesmc8.root':{
+                'observable_ids':{ 'mc_old' :{ 'prediction_index':12, 'spectrum_index':76  }}, 
+                },
             }
