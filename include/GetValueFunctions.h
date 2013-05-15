deb5c9253a198e3d9bd24148be1f40cfd266494c
Keesjan de Vries <keesjan777@gmail.com>
2013-05-15 15:35:12 +0100

    add functions

--- include/GetValueFunctions.h
+++ include/GetValueFunctions.h
@@ -10,4 +10,6 @@ std::map<std::string, GetValueFunction > get_GetValueFunction_map();
 
 ///Individual declarations
 double bsmm_ratio(double *VARS, std::vector<int>* array_ids);
+double difference(double *VARS, std::vector<int>* array_ids);
+double var1_over_var2_square(double *VARS, std::vector<int>* array_ids);
 #endif
--- src/GetValueFunctions.cpp
+++ src/GetValueFunctions.cpp
@@ -2,10 +2,22 @@
 std::map<std::string, GetValueFunction > get_GetValueFunction_map(){
     std::map<std::string, GetValueFunction > function_map;
     function_map["bsmm_ratio"]=bsmm_ratio;
+    function_map["difference"]=difference;
+    function_map["var1_over_var2_square"]=var1_over_var2_square;
     return function_map;
 }
 
 double bsmm_ratio(double *VARS, std::vector<int>* array_ids){
     int bsmm_id=(*array_ids)[0];
     return VARS[bsmm_id]/(3.46e-9);
+}
+
+double difference(double *VARS, std::vector<int>* array_ids){
+    return VARS[(*array_ids)[0]]-VARS[(*array_ids)[1]];
+}
+
+double var1_over_var2_square(double *VARS, std::vector<int>* array_ids){
+    // useful for e.g. mh2/(m0*m0) 
+    double var1=VARS[(*array_ids)[0]], var2=VARS[(*array_ids)[1]];
+    return var1/(var2*var2);
 }
