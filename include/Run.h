95b8a21660e5a773d647d5afedb40e7000f53328
Keesjan de Vries <keesjan777@gmail.com>
2013-05-28 13:46:45 +0100

    this works now some cleanup

---
+++ include/Run.h
@@ -0,0 +1,18 @@
+#ifndef  INC_RUN_H
+#define INC_RUN_H
+#include <iostream>
+#include <sstream>
+#include <cmath>
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TLeaf.h>
+#include <TString.h>
+#include <TH2D.h>
+
+#include "Space.h"
+#include "JsonParsing.h"
+#include "GetValueFunctions.h"
+
+
+#endif
