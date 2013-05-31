662c8f21c6ae7e757725f99cb836819216121fad
Keesjan de Vries <keesjan777@gmail.com>
2013-05-31 16:35:37 +0100

    add gaussfunctions

---
+++ include/GaussFunctions.h
@@ -0,0 +1,15 @@
+#ifndef GAUSSFUNCTIONS_H
+#define GAUSSFUNCTIONS_H
+#include <map>
+#include <string>
+#include <vector>
+#include "GaussConstraint.h" // for GetValueFunction type def
+
+//typedef double(*GaussFunc)(double *, std::vector<int> * , GaussData *); 
+//
+///Key function that returns a map of function pointers
+std::map<std::string, GaussFunc > get_GaussFunc_map();
+
+///Individual declarations
+double gauss(double * VARS, std::vector<int> * array_ids , GaussData * data);
+#endif
---
+++ src/GaussFunctions.cpp
@@ -0,0 +1,11 @@
+#include "GaussFunctions.h"
+std::map<std::string, GaussFunc > get_GaussFunc_map(){
+    std::map<std::string, GaussFunc > gaussfunc_map;
+    gaussfunc_map["gauss"]=gauss;
+    return gaussfunc_map;
+}
+
+double gauss(double * VARS, std::vector<int> * array_ids , GaussData * data){
+    double c =VARS[(*array_ids)[0]];
+    return (data->mu-c)*(data->mu-c)/data->sigma_square;
+} 
