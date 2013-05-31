66ca2106c4d3382f7ac318840533fc8b4a04a23d
Keesjan de Vries <keesjan777@gmail.com>
2013-05-31 16:46:16 +0100

    constraints should have observables ids

--- user/constraints.py
+++ user/constraints.py
@@ -1,9 +1,13 @@
 def get_constraints():
     return {
-    'Mh125':{
-        'gaussian':{
-            'mu':125.,
-            'sigmas':[1.0,2.5],
-            'function':'gaussian',
-        },
+        'Mh125':{
+            'observable_ids' :{
+                'mc_old':'mh',
+            },
+            'gaussian':{
+                'mu':125.,
+                'sigmas':[1.0,2.5],
+                'function_name':'gauss',
+            },
+        }
     }
