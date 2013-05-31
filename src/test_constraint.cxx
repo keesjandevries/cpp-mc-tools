aeef8c39e6b2056c817c26214530f5935d54972e
Keesjan de Vries <keesjan777@gmail.com>
2013-05-31 16:44:24 +0100

    test executables now also have constraints

--- src/test_constraint.cxx
+++ src/test_constraint.cxx
@@ -1,7 +1,7 @@
 #include "Axis.h"
 
 
-double gauss(double * VARS, std::vector<int> * array_ids , GaussData * data){
+double my_gauss(double * VARS, std::vector<int> * array_ids , GaussData * data){
     double c =VARS[(*array_ids)[0]];
     return (data->mu-c)*(data->mu-c)/data->sigma_square;
 } 
@@ -12,7 +12,7 @@ int main(){
     double mu=0.;
     std::vector<double> sigmas;
     sigmas.push_back(1.);
-    GaussConstraint gauss_constraint(array_ids,  mu,  sigmas , gauss);
+    GaussConstraint gauss_constraint(array_ids,  mu,  sigmas , my_gauss);
     double VARS1[]={1.};
     std::cout << "X^2 for 1: "<<gauss_constraint.GetChi2(VARS1) << std::endl;
     double VARS2[]={2.};
