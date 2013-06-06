bd0e54ad3e7725b690e7102271abf3869376f327
Keesjan de Vries <keesjan777@gmail.com>
2013-06-06 17:17:19 +0100

    movig to Contour as a base class

--- include/Contour.h
+++ include/Contour.h
@@ -1,11 +1,10 @@
-#ifndef CONTOUR_H
-#define CONTOUR_H
+#ifndef INC_CONTOUR_H
+#define INC_CONTOUR_H
 #include <iostream>
 #include <map>
 #include <utility>  //std::pair
 #include <vector>
 #include <string>
-#include <cmath>    //atan2()
 
 // define double_pair to reduce syntacs
 typedef std::pair<double,double> double_pair;
@@ -13,46 +12,24 @@ typedef std::pair<double,double> double_pair;
 class Contour{
     public:
         // Directly accesible
+        Contour(){};
         Contour(std::vector<double_pair>/*coords*/);   //FIXME: only coordinates for now
-//        Contour(std::vector<double_pair>/*coords*/, std::string /*par_mode*/);    //interpolate mode is linear
-//        Contour(std::vector<double_pair>/*coords*/, std::string /*par_mode*/,std::string /*interp_mode*/);
+        virtual ~Contour(){};
+        // These are functions that should be available
         double GetContourValue(double /*parameter*/);
-        double GetPointParameter(double_pair /*point_coordinates*/);
         double GetPointParameter(std::vector<double> /*point_coordinates*/);
-        double GetPointValue(double_pair /*point_coordinates*/);
         double GetPointValue(std::vector<double> /*point_coordinates*/);
-
         // FIXME: these should maybe be private memberfunctions
-        std::pair<double_pair,double_pair> GetSegment(double /*parameter*/);
         std::vector<double> GetCoordinatesParameters();
-    private:
-        std::vector<double_pair>    _coordinates;
-        std::vector<double>         _coordinates_parameters;     // e.g. 'x' or 'theta'
-        double (*_get_point_parameter)(double_pair);    //e.g. get_theta(...), get_x(...)
-        double (*_get_point_value)(double_pair);        //e.g. get_radius(...), get_y(...)
-        // functions used by GetContourValue(...)
-        double (*_interpolate)     (double /*parameter*/,std::pair<double_pair,double_pair> /*segment*/);
-        double (*_low_extrapolate) (double /*parameter*/,std::vector<double_pair> * /*coordinates pointer*/);
-        double (*_high_extrapolate)(double /*parameter*/,std::vector<double_pair> * /*coordinates pointer*/);
+    protected:
+        std::vector<double_pair> _coordinates;
+        std::vector<double> _coordinates_parameters;     // e.g. 'x' or 'theta'
+        // virtual member functions
+        virtual double get_point_parameter(double_pair)=0;    //e.g. get_theta(...), get_x(...)
+        virtual double get_point_value(double_pair)=0;        //e.g. get_radius(...), get_y(...)
+        virtual std::pair<double_pair,double_pair> get_segment(double /*parameter*/)=0;
+        virtual double interpolate(double /*parameter*/,std::pair<double_pair,double_pair> /*segment*/)=0;
+        virtual double low_extrapolate(double /*parameter*/)=0;
+        virtual double high_extrapolate(double /*parameter*/)=0;
 };
-
-// functions that do not belong to the class but may be pointed to
-
-/*************************
- * interpolation functions
- ************************/
-double interpolate_x(double parameter, std::pair<double_pair,double_pair> segment);
-
-/*************************
- * extrapolation functions
- ************************/
-double extrapolate_low_x_flat(double parameter,std::vector<double_pair> *);
-double extrapolate_high_x_flat(double parameter,std::vector<double_pair> *);
-
-/*************************
- * get parameter functions
- ************************/
-double get_x(double_pair point_coordinates);
-double get_y(double_pair point_coordinates);
-double get_theta(double_pair point_coordinates);
 #endif
