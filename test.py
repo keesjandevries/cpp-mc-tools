4e8f470fa977d3db4a128178d0f11ce6af558a0f
Keesjan de Vries <keesjan777@gmail.com>
2013-07-12 19:03:00 +0100

    yep

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python
 import test_vars_lookup
+import test_get_entry
 
-my=test_vars_lookup.PyVarsLookup(2)
-print(my.get_value([1,2,3,4,5]))
+my=test_get_entry.PyGetEntry('/vols/cms04/kjd110/budapest_pre_lhc_boxes/cmssm-boxes-combined-pre-lhc.root')
+print( my.get_vars(0))
+#my=test_vars_lookup.PyVarsLookup(2)
+#print(my.get_value([1,2,3,4,5]))
