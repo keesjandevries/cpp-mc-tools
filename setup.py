7103b95b67654c7d282c11bef23b228a5a38eb9b
Keesjan de Vries <keesjan777@gmail.com>
2013-07-12 19:02:38 +0100

    still experimental

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 setup(   
-        ext_modules = cythonize('test_vars_lookup.pyx',include_dirs=['include'])
+#        ext_modules = cythonize('test_vars_lookup.pyx',include_dirs=['include'])
+        ext_modules = cythonize('test_get_entry.pyx',include_dirs=['/usr/include/root/','include',])
 #        ext_modules = cythonize([Extension(
 #            'test_vars_lookup.pyx',
 #            sources=['src/VarsLookup.cpp','src/BaseGetValueFunction.cpp'],
