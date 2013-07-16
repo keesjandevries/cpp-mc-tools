ca6294381577bcba545fd5c05539368ec5191ea4
Keesjan de Vries <keesjan777@gmail.com>
2013-07-16 15:06:41 +0100

    these are the two new files

---
+++ include/RootMakePlots.h
@@ -0,0 +1,24 @@
+#ifndef  INC_ROOTMAKEPLOTS_H
+#define INC_ROOTMAKEPLOTS_H
+//standard library
+#include <vector>
+//root includes
+#include "TFile.h"
+#include "TTree.h"
+#include "TLeaf.h"
+//custom includes
+#include "Space.h"
+class RootMakePlots{
+    public:
+        RootMakePlots(const char *, std::vector<Space*>);
+        virtual ~RootMakePlots();    
+        void Run();
+    private:
+        TFile * _file;
+        TTree * _tree;
+        int _nvars;
+        int _nentries;
+        double * _vars;
+        std::vector<Space*> _spaces; 
+};
+#endif
---
+++ src/RootMakePlots.cpp
@@ -0,0 +1,35 @@
+#include "RootMakePlots.h"
+
+RootMakePlots::RootMakePlots(const char * filename, std::vector<Space*> spaces):
+    _spaces(spaces)
+{
+    _file=new TFile(filename);
+    _tree=(TTree*)_file->Get("tree");// FIXME: hardcoded
+    _nvars=_tree->GetLeaf("vars")->GetLen();
+    _vars=new double[_nvars];
+    _tree->SetBranchAddress("vars",_vars);
+    _nentries = _tree->GetEntries();
+}
+
+RootMakePlots::~RootMakePlots(){
+    delete _vars;
+    delete _file;
+}
+
+void RootMakePlots::Run(){
+    int nentries=_nentries;
+    for(int i=0; i<nentries; i++){
+        //FIXME: make progress bar
+        if (i%100000==0) std::cout << "Processed: " << i << "entries" << std::endl;
+        // get entry
+        _tree->GetEntry(i);
+        ///Update all _spaces: check whether X^2 is lower than existing X^2
+        for( std::vector<Space*>::iterator it=_spaces.begin(); it!=_spaces.end() ; it++){
+            (*it)->update(_vars,i);
+        }
+    }
+    //Write all plots (X^2,entries, *zaxes) to root file
+    for( std::vector<Space*>::iterator it=_spaces.begin(); it!=_spaces.end() ; it++){
+        (*it)->write_plots();
+    }
+}
