9e6ce6de8259cc1f03eb6857fa0c9d13f6816106
Keesjan de Vries <keesjan777@gmail.com>
2013-07-27 22:56:56 +0100

    first setup for Cython wrapper

--- MyCythonWrappers.pyx
+++ MyCythonWrappers.pyx
@@ -2,7 +2,7 @@
 # distutils: include_dirs = include /usr/include/root/
 # distutils: libraries = Core Cint RIO Net Hist Graf Graf3d Gpad Tree Rint Postscript Matrix Physics MathCore Thread  m dl 
 # distutils: library_dirs =  /usr/lib64/root/
-# distutils: sources = src/GetEntry.cpp  src/VarsLookup.cpp src/BaseGetValueFunction.cpp
+# distutils: sources = src/GetEntry.cpp  src/VarsLookup.cpp src/BaseGetValueFunction.cpp src/GetValueManager.cpp
 
 import cython
 from libc.stdlib cimport malloc, free
@@ -52,25 +52,21 @@ cdef class PyVarsLookup:
         value = self.thisptr.get_value(c_vars)
         free( c_vars)
         return value
-
-cdef extern from "VarsFunction.h":
-    cdef cppclass VarsLookup:
-        VarsLookup(int)
-        double get_value(double *)
-
-cdef class PyVarsLookup:
-    cdef VarsLookup *thisptr
-    def __cinit__(self, int array_index):
-        self.thisptr = new VarsLookup(array_index)
-    def __dealloc__(self):
-        del self.thisptr
-    def get_value(self,vars):
-        cdef int len_vars=len(vars)
-        cdef double * c_vars= <double *>malloc(len_vars * cython.sizeof(double))
-        for i in xrange(len(vars)):
-            c_vars[i]=vars[i]
-        cdef double value 
-        value = self.thisptr.get_value(c_vars)
-        free( c_vars)
-        return value
-
+#
+#cdef extern from "GetValueManager.h":
+#    cdef cppclass GetValueManager:
+#        GetValueManager * GetInstance()
+#        void AddVarsLookup(const char*,int)
+#
+#cdef class PyGetValueManager:
+#    cdef GetValueManager * thisptr
+#    def __cinit__(self):
+#        self.thisptr = thisptr
+#        self.thisptr=self.thisptr.GetInstance()
+#    def __dealloc__(self):
+#        del self.thisptr
+#    def add_vars_lookup(self,name,array_id):
+#        byte_name=name.encode('UTF-8')
+#        cdef const char *c_name = byte_name
+#        self.thisptr.AddVarsLookup(byte_name,array_id)
+#        
