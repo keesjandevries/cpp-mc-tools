dc85c6a3510cc793d7a0ffd047e0e476791ef03d
Keesjan de Vries <keesjan777@gmail.com>
2013-07-27 18:00:32 +0100

    cython wrappers in one file

--- MyCythonWrappers.pyx
+++ MyCythonWrappers.pyx
@@ -52,3 +52,25 @@ cdef class PyVarsLookup:
         value = self.thisptr.get_value(c_vars)
         free( c_vars)
         return value
+
+cdef extern from "VarsFunction.h":
+    cdef cppclass VarsLookup:
+        VarsLookup(int)
+        double get_value(double *)
+
+cdef class PyVarsLookup:
+    cdef VarsLookup *thisptr
+    def __cinit__(self, int array_index):
+        self.thisptr = new VarsLookup(array_index)
+    def __dealloc__(self):
+        del self.thisptr
+    def get_value(self,vars):
+        cdef int len_vars=len(vars)
+        cdef double * c_vars= <double *>malloc(len_vars * cython.sizeof(double))
+        for i in xrange(len(vars)):
+            c_vars[i]=vars[i]
+        cdef double value 
+        value = self.thisptr.get_value(c_vars)
+        free( c_vars)
+        return value
+
