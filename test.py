af859e45ab3d6a3703e3661c0807f4b21d473b2b
Keesjan de Vries <keesjan777@gmail.com>
2013-07-27 22:56:31 +0100

    testing ctypes interface

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 #! /usr/bin/env python
 #import test_vars_lookup
-import MyCythonWrappers
+from ctypes import cdll
+#import MyCythonWrappers
+lib=cdll.LoadLibrary('lib/libmylib.so')
 
-my=MyCythonWrappers.PyGetEntry('/vols/cms04/kjd110/budapest_pre_lhc_boxes/cmssm-boxes-combined-pre-lhc.root')
-vars=my.get_vars(0)
-print( vars)
-my=MyCythonWrappers.PyVarsLookup(2)
-print(my.get_value(vars))
+lib.add_vars_lookup('name'.encode('ascii'),5)
+lib.test('name'.encode('ascii'))
+
+
+#my=MyCythonWrappers.PyGetEntry('/vols/cms04/kjd110/budapest_pre_lhc_boxes/cmssm-boxes-combined-pre-lhc.root')
+#vars=my.get_vars(0)
+#print( vars)
+#my=MyCythonWrappers.PyVarsLookup(2)
+#print(my.get_value(vars))
