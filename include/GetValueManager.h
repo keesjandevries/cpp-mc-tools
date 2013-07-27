26df6e9d99bcab9d1c9985187bb5dca0c6c058d2
Keesjan de Vries <keesjan777@gmail.com>
2013-07-27 20:31:29 +0100

    implementation so far

--- include/GetValueManager.h
+++ include/GetValueManager.h
@@ -5,12 +5,18 @@
 #include <string>
 // my own
 #include "BaseGetValueFunction.h"
+#include "VarsLookup.h"
 class GetValueManager {
     public:
-        GetValueManager(){};
-        virtual ~GetValueManager(){};
-        void Add(const char *, BaseGetValueFunction*){};
+        static GetValueManager * GetInstance();
+        ~GetValueManager();
+        void AddVarsLookup(const char *, int);
     private:
+        //private so it cannot be called
+        GetValueManager(){};//constructor
+        GetValueManager(GetValueManager const &); // copy
+        GetValueManager& operator=(GetValueManager const &); //assignment
+        static GetValueManager* _instance; 
         std::map<std::string,BaseGetValueFunction*> _function_map;
 };
 
--- src/GetValueManager.cpp
+++ src/GetValueManager.cpp
@@ -1,1 +1,25 @@
 #include "GetValueManager.h"
+
+GetValueManager * GetValueManager::_instance=NULL;
+
+GetValueManager * GetValueManager::GetInstance(){
+    if (!_instance){
+        _instance=new GetValueManager;
+    }
+    return _instance; 
+}
+
+GetValueManager::~GetValueManager(){
+    // emtpy for now
+    std::map<std::string,BaseGetValueFunction*>::iterator it;
+    for (it=_function_map.begin(); it!=_function_map.begin(); it++){
+        delete it->second;    
+    }
+}
+
+void GetValueManager::AddVarsLookup(const char * name, int array_id){
+    if (_function_map.find(name)!=_function_map.end()){
+        VarsLookup * new_vars_lookup=new VarsLookup(array_id);
+        _function_map[name]=new_vars_lookup;
+    }
+}
