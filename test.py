11c43e28700b1a8ecd1ac45c579ff25572ac5213
Keesjan de Vries <keesjan777@gmail.com>
2013-07-27 18:04:25 +0100

    test script is testing cython wrapping

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
-import test_vars_lookup
-import test_get_entry
+#import test_vars_lookup
+import MyCythonWrappers
 
-my=test_get_entry.PyGetEntry('/vols/cms04/kjd110/budapest_pre_lhc_boxes/cmssm-boxes-combined-pre-lhc.root')
-print( my.get_vars(0))
-#my=test_vars_lookup.PyVarsLookup(2)
-#print(my.get_value([1,2,3,4,5]))
+my=MyCythonWrappers.PyGetEntry('/vols/cms04/kjd110/budapest_pre_lhc_boxes/cmssm-boxes-combined-pre-lhc.root')
+vars=my.get_vars(0)
+print( vars)
+my=MyCythonWrappers.PyVarsLookup(2)
+print(my.get_value(vars))
