69300cc8cdf374c0b316c5e69d89992e4fe6bc87
Keesjan de Vries <keesjan777@gmail.com>
2013-07-29 22:22:35 +0100

    test also includes functions

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 #! /usr/bin/env python
 #import test_vars_lookup
-from ctypes import cdll
+from ctypes import cdll, c_int
 #import MyCythonWrappers
 lib=cdll.LoadLibrary('lib/libmylib.so')
 
+int_array=c_int*2
+array_ids=int_array(0,1)
+
 lib.add_vars_lookup('name'.encode('ascii'),5)
+lib.add_vars_function('name1'.encode('ascii'),array_ids,2,'average'.encode('ascii'))
+
 lib.test('name'.encode('ascii'))
 
 
