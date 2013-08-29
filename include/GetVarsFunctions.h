a827b2dee4ad354d07cd8b2e3f4d50fd1f43be1a
Keesjan de Vries <keesjan777@gmail.com>
2013-08-29 17:11:22 +0100

    add functions for conversion picobarns to square cm and C9 from straub

--- include/GetVarsFunctions.h
+++ include/GetVarsFunctions.h
@@ -10,10 +10,13 @@
 std::map<std::string, GetVarsFunction > get_GetVarsFunction_map();
 
 ///Individual declarations
+double pb_to_cm2(double *VARS, std::vector<int>& array_ids);
 double bsmm_ratio(double *VARS, std::vector<int>& array_ids);
 double difference(double *VARS, std::vector<int>& array_ids);
 double average(double *VARS, std::vector<int>& array_ids);
 double var1_over_var2_square(double *VARS, std::vector<int>& array_ids);
 double var1_over_var2(double *VARS, std::vector<int>& array_ids);
 double sqrt_var1_over_var2(double *VARS, std::vector<int>& array_ids);
+double C9_straub(double * , std::vector<int> & );
+double f9Hpm(double );
 #endif
