24988de41ca79050da69b8fb9a571515e0d824b5
Keesjan de Vries <keesjan777@gmail.com>
2013-08-29 22:35:49 +0100

    introduce reference axis. introduce default reference axis. introduce according constructors

--- include/Space.h
+++ include/Space.h
@@ -3,6 +3,9 @@
 #include "Plot.h"
 #include "Axis.h"
 #include "BaseGetValueFunction.h"
+///FIXME: for compatibility we hard code the chi2 lookup using a vars lookup
+///FIXME: this should ultimately be deprecated
+#include "VarsLookup.h"
 
 class Space{
     public:
@@ -22,16 +25,14 @@ class Space{
     private:
         //private member functions
         void init_reference_plot(std::vector<Axis*>);
+        void init_reference_plot(std::vector<Axis*>,BaseGetValueFunction*);
         void init_entry_plot(std::vector<Axis*>);
         void init_other_plots(std::vector<Axis*>,std::vector<Axis*>);
         //private member objects
         //plots
-        Plot*               _reference_plot;
-        Plot*               _entry_plot;
-        std::vector<Plot*>  _other_plots;
-        //default values, set in constructor
-        int _chi2_index;
-        double _reference_init_default;
-        double _entry_init_default;
+        Plot* _reference_plot;
+        Plot* _entry_plot;
+        std::vector<Plot*> _other_plots;
+        Axis* _reference_zaxis;
 };
 #endif
--- src/Space.cpp
+++ src/Space.cpp
@@ -4,25 +4,41 @@ Space::Space(std::vector<Axis*> axes){
     init_entry_plot(axes);
 }
 
+Space::Space(std::vector<Axis*> axes,BaseGetValueFunction * reference_function){
+    init_reference_plot(axes,reference_function); 
+    init_entry_plot(axes);
+}
+
 Space::Space(std::vector<Axis*> axes,std::vector<Axis*> zaxes){
     init_reference_plot(axes);
     init_entry_plot(axes);
     init_other_plots(axes,zaxes);
 }
 
+Space::Space(std::vector<Axis*> axes,std::vector<Axis*> zaxes,BaseGetValueFunction * reference_function){
+    init_reference_plot(axes,reference_function); 
+    init_entry_plot(axes);
+    init_other_plots(axes,zaxes);
+}
+
 void Space::init_reference_plot(std::vector<Axis *> axes){
-    _reference_init_default=1e9; //FIXME: global variable here?
-    _chi2_index=0;
-    Axis * base_chi2_zaxis = new Axis("chi2");
-    _reference_plot = new Plot(axes,base_chi2_zaxis);
-    _reference_plot->fill_default(_reference_init_default);
+    /// For compatibility the default reference function is 
+    /// the vars lookup at index 0.
+    /// Traditionally we minimise chi2 which is stored at index 0.
+    VarsLookup * default_reference_function=new VarsLookup(0);
+    init_reference_plot(axes,default_reference_function); 
+}
+
+void Space::init_reference_plot(std::vector<Axis *> axes, BaseGetValueFunction * reference_function){
+    _reference_zaxis = new Axis("chi2",reference_function);
+    _reference_plot = new Plot(axes,_reference_zaxis);
+    _reference_plot->fill_default(1e9);
 }
 
 void Space::init_entry_plot(std::vector<Axis *> axes){
-    double _entry_init_default=-1.;// makes sense to have -1 as  marker for invalid entry number
     Axis * entry_zaxis = new Axis("entries" );
     _entry_plot = new Plot(axes,entry_zaxis);
-    _entry_plot->fill_default(_entry_init_default);
+    _entry_plot->fill_default(-1);
 }
 
 void Space::init_other_plots(std::vector<Axis *> axes, std::vector<Axis *> zaxes){
@@ -34,9 +50,9 @@ void Space::init_other_plots(std::vector<Axis *> axes, std::vector<Axis *> zaxes
 
 void Space::update(double * VARS, int entry_nr){
     int ibin=_reference_plot->find_bin(VARS);
-    double chi2=VARS[_chi2_index];
-    if(chi2<_reference_plot->get_bin_content(ibin)){
-        _reference_plot->set_bin_content(ibin,chi2);
+    double reference=_reference_zaxis->get_value(VARS);
+    if(reference<_reference_plot->get_bin_content(ibin)){
+        _reference_plot->set_bin_content(ibin,reference);
         _entry_plot->set_bin_content(ibin,(double)entry_nr);
         for (std::vector<Plot*>::iterator it=_other_plots.begin(); it!=_other_plots.end(); it++){
             (*it)->set_bin_content(ibin,VARS);
