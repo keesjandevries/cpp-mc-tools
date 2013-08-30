a0674e5b9114ceef422ee4c3c7a4da133f381799
Keesjan de Vries <keesjan777@gmail.com>
2013-08-30 16:06:02 +0100

    add space manager

---
+++ include/SpaceManager.h
@@ -0,0 +1,25 @@
+#ifndef INCLUDE_SPACEMANAGER_H_
+#define INCLUDE_SPACEMANAGER_H_
+// standard library
+#include <iostream>
+#include <map>
+#include <string>
+// my own
+
+#include "Space.h"
+#include "AxisManager.h"
+class SpaceManager {
+    public:
+        static SpaceManager * GetInstance();
+        ~SpaceManager();
+        void AddSpace(std::vector<std::string>, std::vector<std::string>, std::string);
+    private:
+        //private so it cannot be called
+        SpaceManager(){};//constructor
+        SpaceManager(SpaceManager const &); // copy
+        SpaceManager& operator=(SpaceManager const &); //assignment
+        static SpaceManager* _instance; 
+        std::vector<Space*> _space_vector;
+};
+
+#endif // INCLUDE_SPACEMANAGER_H_
