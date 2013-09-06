75786ad60c4a60ba07dd4c50a040b1e22052323b
Keesjan de Vries <keesjan777@gmail.com>
2013-09-06 18:20:41 +0100

    edit gauss_constraints

--- user/gauss_constraints.py
+++ user/gauss_constraints.py
@@ -3,23 +3,24 @@ def get():
             'Mh125': {
                 'function_name': 'gauss',
                 'mu': 125.0,
-                'sigmas': [1.0, 2.5]
+                'sigmas': [1.0, 2.5],
                 'observable_ids': {
                     'mc_old': 'mh'
                     },
                 },
-            'Oh2': {'function_name': 'gauss',
+            'chi2Oh2': {
+                'function_name': 'gauss',
                 'mu': 0.1109,
+                'sigmas': [0.0056, 0.012],
                 'observable_ids': {
                     'mc_old': 'Oh^2',
                     'mcpp': ('Micromegas', 'Omega')
                     },
-                'sigmas': [0.0056, 0.012]
                 },
             'g-2': {
                 'function_name': 'gauss',
                 'mu': 3.02e-09,
-                'sigmas': [8.8e-10, 2e-10]
+                'sigmas': [8.8e-10, 2e-10],
                 'observable_ids': {
                     'mc_old': 'Delta(g-2)'
                     },
