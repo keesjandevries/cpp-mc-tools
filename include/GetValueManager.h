1a9371a53a5235a92a6d15f26709c15021182107
Keesjan de Vries <keesjan777@gmail.com>
2013-09-06 17:15:57 +0100

    add Gauss constraint to GetValueManager

--- include/GetValueManager.h
+++ include/GetValueManager.h
@@ -9,6 +9,8 @@
 #include "VarsLookup.h"
 #include "VarsFunction.h"
 #include "GetVarsFunctions.h"
+#include "GaussConstraint.h"
+#include "GaussFunctions.h"
 #include "Chi2Calculator.h"
 
 class GetValueManager {
@@ -18,6 +20,7 @@ class GetValueManager {
         void AddVarsLookup(const char *, int);
         void AddVarsFunction(const char *, std::vector<int>, const char *);
         void AddVarsFunction(const char *, int*, int, const char *);
+        void AddGaussConstraint(const char *, std::vector<int>, double, std::vector<double>, const char *);
         // FIXME: perhaps separating out the Chi2Calculator
         void AddChi2Calculator(const char *);
         void AddConstraintToChi2Calculator(const char *,const char *);
@@ -31,6 +34,7 @@ class GetValueManager {
         std::map<std::string,BaseGetValueFunction*> _function_map;
         std::map<std::string,Chi2Calculator*> _chi2_calculator_map;
         std::map<std::string,GetVarsFunction> _get_vars_function_map;
+        std::map<std::string,GaussFunc> _gauss_func_map;
 };
 
 #endif
