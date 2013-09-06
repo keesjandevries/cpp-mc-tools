74cdb0ec82be5a6d2f3fa1f506810d2b5035fa09
Keesjan de Vries <keesjan777@gmail.com>
2013-09-06 17:14:46 +0100

    made data for gauss constraint contain a vector of sigmas, to allow for Higgs and asymmetric gaussians

--- src/GaussFunctions.cpp
+++ src/GaussFunctions.cpp
@@ -5,7 +5,17 @@ std::map<std::string, GaussFunc > get_GaussFunc_map(){
     return gaussfunc_map;
 }
 
+double sum_squares(std::vector<double> sigmas){
+    double sum=0;
+    std::vector<double>::iterator it;
+    for (it=sigmas.begin();it!=sigmas.end();it++){
+        sum+=(*it)*(*it);
+    }
+    return sum;
+}
+
 double gauss(double * VARS, std::vector<int> & array_ids , GaussData & data){
-    double c =VARS[array_ids[0]];
-    return (data.mu-c)*(data.mu-c)/data.sigma_square;
+    double c=VARS[array_ids[0]];
+    double sigma_squared=sum_squares(data.sigmas);
+    return (data.mu-c)*(data.mu-c)/sigma_squared;
 } 
