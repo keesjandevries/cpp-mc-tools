8af52d719a819af33c4bcfbf0da49fa8fad42f1e
Keesjan de Vries <keesjan777@gmail.com>
2013-09-07 17:45:21 +0100

    working implementation of contour manager

--- include/ContourManager.h
+++ include/ContourManager.h
@@ -6,17 +6,24 @@
 #include <string>
 // my own
 #include "Contour.h"
+#include "DefaultContour.h"
+#include "LogXLogYContour.h"
+#include "UniversalLimitsContour.h"
 
 class ContourManager {
     public:
         static ContourManager * GetInstance();
         ~ContourManager();
+        void AddContour(const char *,std::vector<double_pair>,const char *);
+        void AddContour(const char *,double*,double*,int,const char *);
+        Contour * Get(const char *);
     private:
         //private so it cannot be called
         ContourManager(){};//constructor
         ContourManager(ContourManager const &); // copy
         ContourManager& operator=(ContourManager const &); //assignment
         static ContourManager* _instance; 
+        std::map<std::string,Contour*> _contour_map;
 };
 
 #endif // INCLUDE_CONTOURMANAGER_H
