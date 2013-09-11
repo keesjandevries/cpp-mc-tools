2bf16ab719e2eb09cc85aaca372eb3be7c29b502
Keesjan de Vries <keesjan777@gmail.com>
2013-09-11 18:31:30 +0100

    added and tested all functions that were already in mcpp

--- include/GaussFunctions.h
+++ include/GaussFunctions.h
@@ -1,5 +1,6 @@
 #ifndef GAUSSFUNCTIONS_H
 #define GAUSSFUNCTIONS_H
+#include <cmath>
 #include <map>
 #include <string>
 #include <vector>
@@ -12,5 +13,16 @@ std::map<std::string, GaussFunc > get_GaussFunc_map();
 ///Auxiliry funcitons
 double sum_squares(std::vector<double>);
 ///Individual declarations
-double gauss(double * VARS, std::vector<int> & array_ids , GaussData & data);
+double gauss(double * , std::vector<int> &  , GaussData & );
+double asymmetric_gauss(double * , std::vector<int> &  , GaussData & );
+double ratio_gauss(double * , std::vector<int> &  , GaussData & );
+double bsmm_ratio_gauss(double * , std::vector<int> &  , GaussData & );
+double R_bsmm_chi2(double * , std::vector<int> &  , GaussData & );
+double higgs_gauss(double * , std::vector<int> &  , GaussData & );
+double lowerlimit(double * , std::vector<int> &  , GaussData & );
+double abs_lowerlimit(double * , std::vector<int> &  , GaussData & );
+double upperlimit(double * , std::vector<int> &  , GaussData & );
+double multi_lowerlimit(double * , std::vector<int> &  , GaussData & );
+double multi_abs_lowerlimit(double * , std::vector<int> &  , GaussData & );
+double neutralino_lsp(double * , std::vector<int> &  , GaussData & );
 #endif
