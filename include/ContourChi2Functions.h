65538292a338ea3bfa9c4bd613f571dec67eb8c4
Keesjan de Vries <keesjan777@gmail.com>
2013-09-11 20:18:08 +0100

    implemented and tested xenon100 and ma_tanb

--- include/ContourChi2Functions.h
+++ include/ContourChi2Functions.h
@@ -9,9 +9,10 @@
 
 std::map< std::string, ContourFunc>  get_ContourFunc_map();
 
-double ma_tanb_mc8(double * , std::vector<int> & array_ids, std::vector<Contour*> &);
-double xenon100_mc8(double * , std::vector<int> & array_ids, std::vector<Contour*> &);
-double universal_limits(double * , std::vector<int> & array_ids, std::vector<Contour*> &);
-double m3g_universal_limits(double * , std::vector<int> & array_ids, std::vector<Contour*> &);
-double mg_universal_limits(double * , std::vector<int> & array_ids, std::vector<Contour*> &);
+double mc8_ma_tanb(double * , std::vector<int> & , std::vector<Contour*> &);
+double xenon100_jul_2012(double * , std::vector<int> & , std::vector<Contour*> &);
+double xenon100_jul_2012_Sigma_pi_N_unc(double * , std::vector<int> & , std::vector<Contour*> &);
+//double universal_limits(double * , std::vector<int> & , std::vector<Contour*> &);
+//double m3g_universal_limits(double * , std::vector<int> & , std::vector<Contour*> &);
+//double mg_universal_limits(double * , std::vector<int> & , std::vector<Contour*> &);
 #endif
