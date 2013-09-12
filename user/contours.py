6cc00fd0a7baf2231cfc73889967066c36dc6d0e
Keesjan de Vries <keesjan777@gmail.com>
2013-09-12 13:24:50 +0100

    add ma_tanb contour

--- user/contours.py
+++ user/contours.py
@@ -3,5 +3,9 @@ def get():
             'xenon100_july_2012':{
                     'type':'log_x_log_y',
                     'file':'user/data_files/xenon100_july_1012.dat',
+                },
+            'mc8_ma_tanb':{
+                    'type':'default',
+                    'file':'user/data_files/mc8_ma_tanb.dat',
                 }
             }
