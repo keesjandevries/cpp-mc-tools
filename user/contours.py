d605080298a94df0984127ebebcf57e924299a94
Keesjan de Vries <keesjan777@gmail.com>
2013-09-12 15:07:35 +0100

    add 2 m0-m12 and one bsmm contours

--- user/contours.py
+++ user/contours.py
@@ -7,5 +7,17 @@ def get():
             'mc8_ma_tanb':{
                     'type':'default',
                     'file':'user/data_files/mc8_ma_tanb.dat',
+                },
+            'atlas_5fb_m0_m12':{
+                    'type':'radial',
+                    'file':'user/data_files/atlas_5fb_m0_m12.dat'
+                },
+            'atlas_20fb_m0_m12':{
+                    'type':'radial',
+                    'file':'user/data_files/atlas_20fb_m0_m12.dat'
+                },
+            'mc8_bsmm':{
+                    'type':'default',
+                    'file':'user/data_files/mc8_bsmm.dat',
                 }
             }
