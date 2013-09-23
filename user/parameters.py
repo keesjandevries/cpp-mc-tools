e7450a797b8f4236d73af0ed6fed6d7af4c53b03
Keesjan de Vries <keesjan777@gmail.com>
2013-09-23 10:26:47 +0100

    add new option selectors for observables and parameters used in point.py

---
+++ user/parameters.py
@@ -0,0 +1,7 @@
+def get(name):
+    return parameters.get(name)
+
+parameters={
+        'cmssm':['m0','m12','tanb','A0','mtop','mz','Delta_alpha_had' ],
+        'nuhm1':['m0','m12','tanb','A0','mh2','mtop','mz','Delta_alpha_had' ],
+        }
