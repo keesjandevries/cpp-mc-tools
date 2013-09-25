b3aedaf7b19192a4e673d3cf72a1d5607b95d769
Keesjan de Vries <keesjan777@gmail.com>
2013-09-25 19:35:14 +0100

    add cuts to run function

--- src/RootMakePlots.cpp
+++ src/RootMakePlots.cpp
@@ -46,10 +46,6 @@ RootMakePlots::~RootMakePlots(){
     delete _chain;
 }
 
-void RootMakePlots::Run(){
-    int nentries=_nentries;
-    Run(nentries);
-}
 
 void RootMakePlots::init_root_file(const char * filename){
     std::vector<const char *> filenames;
@@ -69,16 +65,46 @@ void RootMakePlots::init_root_files(std::vector<const char *> filenames){
     _nentries = _chain->GetEntries();
 }
 
+void RootMakePlots::Run(){
+    int nentries=_nentries;
+    Run(nentries);
+}
+
+void RootMakePlots::Run(std::vector<Cut*> cuts){
+    int nentries=_nentries;
+    Run(nentries,cuts);
+}
+
 void RootMakePlots::Run(int nentries){
+    std::vector<Cut*> no_cuts;
+    Run(nentries,no_cuts);
+}
+
+void RootMakePlots::Run(int nentries, std::vector<Cut*> cuts){
     std::cout << "Plotting " << nentries << "points" << std::endl; 
+    //initiate iterators over cuts and spaces outside for loop
+    std::vector<Cut*>::iterator cut_it;
+    std::vector<Space*>::iterator space_it;
     for(int i=0; i<nentries; i++){
         // print progress bar
         loadbar(i,nentries);
+        // check for cuts: if a cut function returns true, then go to the next entry
+        bool passed_cuts=true;
+        for (cut_it=cuts.begin();cut_it!=cuts.end();cut_it++){
+            // NOTE: *cut_it is a pointer, therefore dereference
+            if ((*(*cut_it))(_vars)){
+                passed_cuts=false;
+                break;
+            }
+        }
+        if (!passed_cuts){
+            continue;
+        }
         // get entry
         _chain->GetEntry(i);
         ///Update all _spaces: check whether X^2 is lower than existing X^2
-        for( std::vector<Space*>::iterator it=_spaces.begin(); it!=_spaces.end() ; it++){
-            (*it)->update(_vars,i);
+        for( space_it=_spaces.begin(); space_it!=_spaces.end() ; space_it++){
+            (*space_it)->update(_vars,i);
         }
     }
     //Write all plots (X^2,entries, *zaxes) to root file
