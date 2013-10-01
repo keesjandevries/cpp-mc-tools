6c935b300587c5960eed2114112fbc1f4a785e71
Keesjan de Vries <keesjan777@gmail.com>
2013-10-01 12:28:13 +0100

    add some cuts

--- include/CutFunctions.h
+++ include/CutFunctions.h
@@ -1,5 +1,6 @@
 #ifndef CUTFUNCTIONS_H
 #define CUTFUNCTIONS_H
+#include <iostream> //for debugging
 #include <cmath>
 #include <map>
 #include <string>
@@ -8,5 +9,8 @@
 
 ///Key function that returns a map of function pointers
 std::map<std::string, CutFunction > get_CutFunction_map();
+bool predictor_error(double *, std::vector<int> &);
+bool chi2_error(double *, std::vector<int> &);
+
 #endif // CUTFUNCTIONS_H
 
