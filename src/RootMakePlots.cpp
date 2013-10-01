70381f7c6a51a011109378fdd80b7fb807d7b38a
Keesjan de Vries <keesjan777@gmail.com>
2013-10-01 12:31:04 +0100

    add progress bar and introduce cuts

--- src/RootMakePlots.cpp
+++ src/RootMakePlots.cpp
@@ -2,17 +2,25 @@
 
 // This progress bar is literally taken from 
 // http://www.rosshemsley.co.uk/2011/02/creating-a-progress-bar-in-c-or-any-other-console-app/
-static inline void loadbar(unsigned int x, unsigned int n, unsigned int w =50)
+static inline void loadbar(int x, int n, int w =50)
 {
-    if ( (x != n) && (x % (n/100) != 0) ) return;
- 
-    float ratio  =  x/(float)n;
-    int   c      =  ratio * w;
- 
-    std::cout << std::setw(3) << (int)(ratio*100) << "% [";
-    for (int x=0; x<c; x++) std::cout << "=";
-    for (int x=c; x<w; x++) std::cout << " ";
-    std::cout << "]\r" << std::flush;
+    if ( n<100 || ((x != n) && (x % (n/100) != 0)) ) return;
+    else if (x != n){ 
+        float ratio  =  x/(float)n;
+        int   c      =  ratio * w;
+     
+        std::cout << std::setw(3) << (int)(ratio*100) << "% [";
+        for (int x=0; x<c; x++) std::cout << "=";
+        for (int x=c; x<w; x++) std::cout << " ";
+        std::cout << "]\r" << std::flush;
+        return;
+    }
+    else{
+        std::cout << std::setw(3) << 100 << "% [";
+        for (int x=0; x<w; x++) std::cout << "=";
+        std::cout << "]\r" << std::endl;
+        return;
+    } 
 }
 
 RootMakePlots::RootMakePlots(const char * filename, std::vector<Space*> spaces):
@@ -81,32 +89,34 @@ void RootMakePlots::Run(int nentries){
 }
 
 void RootMakePlots::Run(int nentries, std::vector<Cut*> cuts){
-    std::cout << "Plotting " << nentries << "points" << std::endl; 
+    std::cout << "Processing " << nentries << "points" << std::endl; 
     //initiate iterators over cuts and spaces outside for loop
     std::vector<Cut*>::iterator cut_it;
     std::vector<Space*>::iterator space_it;
+    int cut_counter=0;
     for(int i=0; i<nentries; i++){
-        // print progress bar
-        loadbar(i,nentries);
+        // get entry
+        _chain->GetEntry(i);
         // check for cuts: if a cut function returns true, then go to the next entry
         bool passed_cuts=true;
         for (cut_it=cuts.begin();cut_it!=cuts.end();cut_it++){
             // NOTE: *cut_it is a pointer, therefore dereference
             if ((*(*cut_it))(_vars)){
                 passed_cuts=false;
+                cut_counter++;
                 break;
             }
         }
-        if (!passed_cuts){
-            continue;
+        if (passed_cuts){
+            ///Update all _spaces: check whether X^2 is lower than existing X^2
+            for( space_it=_spaces.begin(); space_it!=_spaces.end() ; space_it++){
+                (*space_it)->update(_vars,i);
+            }
         }
-        // get entry
-        _chain->GetEntry(i);
-        ///Update all _spaces: check whether X^2 is lower than existing X^2
-        for( space_it=_spaces.begin(); space_it!=_spaces.end() ; space_it++){
-            (*space_it)->update(_vars,i);
-        }
+        // print progress bar
+        loadbar(i,nentries-1);
     }
+    std::cout << "Number of point cut out: " << cut_counter << std::endl; 
     //Write all plots (X^2,entries, *zaxes) to root file
     if (_outfile->cd(_outdir)!=kTRUE){
         _outfile->mkdir(_outdir);
