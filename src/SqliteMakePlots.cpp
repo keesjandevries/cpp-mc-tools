9010f5fc0e1e90d1538b609332c4782796c528ba
Keesjan de Vries <keesjan777@gmail.com>
2013-11-06 13:47:56 +0000

    remove test output statements and FIXMEs

--- src/SqliteMakePlots.cpp
+++ src/SqliteMakePlots.cpp
@@ -1,5 +1,4 @@
 #include <SqliteMakePlots.h>
-#include <ctime>
 
 SqliteMakePlots::SqliteMakePlots(const char * filename){
     int error;
@@ -10,44 +9,38 @@ SqliteMakePlots::SqliteMakePlots(const char * filename){
 }
 
 void SqliteMakePlots::Run(const char * query, std::vector<Space*> spaces){
-    //FIXME: remove, this is only for testing
-    clock_t t1,t2,t3;
     /// Prepare sql statement
     int error;
     const char * tail;
     sqlite3_stmt * stmt;
-    t1=clock();
-    error = sqlite3_prepare(_connection,"select * from points;",1000,&stmt,&tail);
-    t2=clock();
-    std::cout << "SQL preparation took: " << ((float)(t2-t1))/CLOCKS_PER_SEC << " seconds" << std::endl;
-
+    error = sqlite3_prepare(_connection,"select rowid, * from points;",1000,&stmt,&tail);
     if (error != SQLITE_OK){
         std::cout << "ERROR: prepare stament failed" << std::endl;
         return;
     }
     /// for retrieving tha vars
-    //FIXME: get proper number of columns
-    int ncols=2;
-    double vars[1000];
-    //FIXME: get proper row number
-    int row_nr=0;
+    int rowid;
+    int ncols=sqlite3_column_count(stmt);
+    double *vars = new double[ncols];
     /// Space iterator
     std::vector<Space*>::iterator space_it;
+    /// loop over selected rows
+    int nrows=0;
     while (sqlite3_step(stmt)==SQLITE_ROW){
+        if (nrows%100000==0) std::cout << "Currently at " << nrows << " rows\r" << std::flush;
         ///Get all variables
-        for (int i=0;i<ncols;i++){ 
-            vars[i]=sqlite3_column_double(stmt,i);
+        rowid=sqlite3_column_int(stmt,0);
+        for (int i=0;i<ncols-2;i++){ 
+            vars[i]=sqlite3_column_double(stmt,i+2);
         }
         ///Update all _spaces: check whether X^2 is lower than existing X^2
         for( space_it=spaces.begin(); space_it!=spaces.end() ; space_it++){
-            (*space_it)->update(vars,row_nr);
+            (*space_it)->update(vars,rowid);
         }
-        row_nr++;
+        nrows++;
     }
-    t3=clock();
-    std::cout << "Looping over results took : " << ((float)(t3-t2))/CLOCKS_PER_SEC << " seconds" << std::endl;
     sqlite3_finalize(stmt);
     sqlite3_close(_connection);
-    //FIXME: for now use row nr for row counting
-    std::cout << "Number of rows processed: " << row_nr << std::endl;
+    std::cout << "Number of rows processed: " << nrows << std::endl;
+    delete vars;
 }
