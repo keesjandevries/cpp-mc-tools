862dcc45813df0898545d43334a0c37ea27c60ba
Keesjan de Vries <keesjan777@gmail.com>
2014-02-10 10:08:05 +0000

    count number of points with chi2 lt 45

--- src/SqliteMakePlots.cpp
+++ src/SqliteMakePlots.cpp
@@ -8,7 +8,8 @@ SqliteMakePlots::SqliteMakePlots(const char * filename){
     }
 }
 
-void SqliteMakePlots::Run(const char * query,int query_length, std::vector<Space*> spaces){
+void SqliteMakePlots::Run(const char * query,int query_length, std::vector<Space*> spaces, 
+        BaseGetValueFunction * reference_function){
     ///FIXME: atm the 'collection_rowid' columns has to be selected. This should be only optional
     /// Prepare sql statement
     int error;
@@ -25,8 +26,10 @@ void SqliteMakePlots::Run(const char * query,int query_length, std::vector<Space
     double *vars = new double[ncols-2];
     /// Space iterator
     std::vector<Space*>::iterator space_it;
+    /// some definitions
+    int nrows=0, n_chi2_lt_45=0;
+    double reference;
     /// loop over selected rows
-    int nrows=0;
     while (sqlite3_step(stmt)==SQLITE_ROW){
         if (nrows%100000==0) std::cout << "Currently at " << nrows << " rows\r" << std::flush;
         ///Get all variables
@@ -34,14 +37,18 @@ void SqliteMakePlots::Run(const char * query,int query_length, std::vector<Space
         for (int i=0;i<ncols-2;i++){ 
             vars[i]=sqlite3_column_double(stmt,i+2);
         }
+        ///Calculate reference (usually the chi2)
+        reference=(*reference_function)(vars);
+        if (reference<45) n_chi2_lt_45++;
         ///Update all _spaces: check whether X^2 is lower than existing X^2
         for( space_it=spaces.begin(); space_it!=spaces.end() ; space_it++){
-            (*space_it)->update(vars,rowid);
+            (*space_it)->update(vars,rowid,reference);
         }
         nrows++;
     }
     sqlite3_finalize(stmt);
     sqlite3_close(_connection);
-    std::cout << "Number of rows processed: " << nrows << std::endl;
+    std::cout << "Number of rows processed:       " << nrows << std::endl;
+    std::cout << "Number of points with chi2<45 : " << n_chi2_lt_45 << std::endl;
     delete vars;
 }
