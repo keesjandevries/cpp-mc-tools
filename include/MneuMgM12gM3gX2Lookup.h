8d165a2052c1b2cf1ecaa7e0947b039ece4ac219
Keesjan de Vries <keesjan777@gmail.com>
2014-04-04 14:39:14 +0100

    3d interpolation is now working

--- include/MneuMgM12gM3gX2Lookup.h
+++ include/MneuMgM12gM3gX2Lookup.h
@@ -1,16 +1,23 @@
 #ifndef INC_MNEUMGM12GM3GX2LOOKUP_H
 #define INC_MNEUMGM12GM3GX2LOOKUP_H
+#include <iostream>
+#include <vector>
+#include <algorithm>
 #include "BaseGetValueFunction.h"
 
 class MgM12gM3gX2Lookup{
     public:
         MgM12gM3gX2Lookup(){};
         MgM12gM3gX2Lookup(double * /*masses_3d*/, int nmg, int nm12g, int nm3g);
-        virtual ~MgM12gM3gX2Lookup(){};
+        MgM12gM3gX2Lookup(double * /*X2*/, double /*default_X2*/, std::vector<double> /*grid_mg*/,
+            std::vector<double> /*grid_m12g*/, std::vector<double> /*grid_m3g*/);
+        virtual ~MgM12gM3gX2Lookup();
         double get_X2(double,double,double);
     private:
         int _nmg, _nm12g, _nm3g;
-        std::vector<double> _mg_m12g_m3g_ranges;
+        double* _X2;
+        double _default_X2;
+        std::vector<std::vector<double> > _mg_m12g_m3g_ranges;
 };
 
 class MneuMgM12gM3gX2Lookup: public BaseGetValueFunction{
--- src/MneuMgM12gM3gX2Lookup.cpp
+++ src/MneuMgM12gM3gX2Lookup.cpp
@@ -1,77 +1,88 @@
 #include "MneuMgM12gM3gX2Lookup.h"
 
 
-MgM12gM3gX2Lookup::MgM12gM3gX2Lookup(double * 3d_masses, int nmg, int nm12g, int nm3g):
-    _nmg(nmg), _nm12g(nm12g), _nm3g(nm3g)
+MgM12gM3gX2Lookup::MgM12gM3gX2Lookup(double * X2, double default_X2, std::vector<double> grid_mg,
+    std::vector<double> grid_m12g, std::vector<double> grid_m3g):
+    _default_X2(default_X2)
 {
-    std::vector<double> grid_mg(nmg);
-    std::vector<double> grid_m12g(nm12g);
-    std::vector<double> grid_m3g(nm3g);
-    for (int i=0; i<nmg; i++){  
-        grid_mg[i]=masses[i*nm12g*nm3g*3+1];
-    }
-    for (int i=0; i<nm12g; i++){
-        grid_m12g[i]=masses[i*nm3g*3+2];
-    }
-    for (int i=0; i<nm3g; i++){
-        grid_m3g[i]=masses[i*3+3];
-    }
+    _nmg=grid_mg.size();
+    _nm12g=grid_m12g.size();
+    _nm3g=grid_m3g.size();
     _mg_m12g_m3g_ranges.push_back(grid_mg);
     _mg_m12g_m3g_ranges.push_back(grid_m12g);
     _mg_m12g_m3g_ranges.push_back(grid_m3g);
+    _X2=new double[_nmg*_nm12g*_nm3g];
+    for (int i=0; i<_nmg*_nm12g*_nm3g; i++){
+        _X2[i]=X2[i];
+    }
+}
+MgM12gM3gX2Lookup::~MgM12gM3gX2Lookup(){
+    delete  _X2;
 }
 
-MgM12gM3gX2Lookup::get_X2(double mg, double m12g, double m3g){
-
+double MgM12gM3gX2Lookup::get_X2(double mg, double m12g, double m3g){
     // helper variables
     double m[3];
     m[0]=mg; m[1]=m12g; m[2]=m3g;
     int lower_index[3];
-    double c[4][2];
+    double c[3][2];
     bool outside;
     int i_mg,i_m12g,i_m3g; 
     std::vector<double>::iterator m_it;
+    std::vector<double>::iterator m_begin;
+    std::vector<double>::iterator m_end;
     //result
     double X2_interp=0;
     for (int i=0;i<3;i++){
-        m_it=std::upper_bound(_mg_m12g_m3g_ranges[i]);
+    //    std::cout << *_mg_m12g_m3g_ranges[i].begin() << std::endl;
+    //    std::cout << *(--_mg_m12g_m3g_ranges[i].end()) << std::endl;
+        m_begin=_mg_m12g_m3g_ranges[i].begin();
+        m_end=_mg_m12g_m3g_ranges[i].end();
+        m_it=std::upper_bound(m_begin,m_end,m[i]);
         outside=(m_it==_mg_m12g_m3g_ranges[i].begin()) || (m_it==_mg_m12g_m3g_ranges[i].end());
         if (!outside){
             c[i][0]=c1(m_it,m[i]);
             c[i][1]=c2(m_it,m[i]);
-            lower_index[i]=(m_it-_mg_m12g_m3g_ranges[i].begin());
+            lower_index[i]=(m_it-_mg_m12g_m3g_ranges[i].begin()-1);
         }
         else{
             //FIXME: remove print statement 
-            std::cerr << "m[" << i << "] = " << m[i] << " out of range" << std::endl;
+            //std::cerr << "m[" << i << "] = " << m[i] << " out of range" << std::endl;
             break;
         }
         //FIXME: remove print statement
-        std::cout << "lower_index[" << i << "]: " << lower_index[i] << std::endl; 
+    //    std::cout << "lower_index[" << i << "]: " << lower_index[i] << std::endl; 
     }
     if (outside){
-        std::cerr << "one of the masses was outside the ranges\nExiting" <<   std::endl;
-        return _default;
+    //    std::cerr << "one of the masses was outside the ranges\nExiting" <<   std::endl;
+        return _default_X2;
     }
+    double temp;
     for (int a_mg=0; a_mg<2; a_mg++){
         for (int a_m12g=0; a_m12g<2; a_m12g++){
             for (int a_m3g=0; a_m3g<2; a_m3g++){
-                i_mg=lower_index[0]+a_mg; i_m12g=lower_index[0]+a_m12g; i_m3g=lower_index[0]+a_m3g;
-                X2_interp+=c[0][a_mg]*c[1][a_m12g]*c[2][a_m3g]*X2[i_mg*nm12g*nm3g+i_m12g*nm3g+i_m3g];
+                i_mg=lower_index[0]+a_mg; i_m12g=lower_index[1]+a_m12g; i_m3g=lower_index[2]+a_m3g;
+                temp=c[0][a_mg]*c[1][a_m12g]*c[2][a_m3g]*_X2[i_mg*_nm12g*_nm3g+i_m12g*_nm3g+i_m3g];
+                //std::cout << " mg: " << _mg_m12g_m3g_ranges[0][i_mg]
+                //          << " c_mg: " << c[0][a_mg]
+                //          << " m12g: " << _mg_m12g_m3g_ranges[1][i_m12g]
+                //          << " c_m12g: " << c[1][a_m12g]
+                //          << " m3g: " << _mg_m12g_m3g_ranges[2][i_m3g]
+                //          << " c_m3g: " << c[2][a_m3g]
+                //          << " X2: " << _X2[i_mg*_nm12g*_nm3g+i_m12g*_nm3g+i_m3g] 
+                //          << " X2_eff: " << temp << std::endl;
+                X2_interp+=c[0][a_mg]*c[1][a_m12g]*c[2][a_m3g]*_X2[i_mg*_nm12g*_nm3g+i_m12g*_nm3g+i_m3g];
             }
         }
     }
     return X2_interp;  
 }
 
-MneuMgM12gM3gX2Lookup::MneuMgM12gM3gX2Lookup(int array_id): 
-    _array_id(array_id)
-{
+MneuMgM12gM3gX2Lookup::MneuMgM12gM3gX2Lookup(int array_id){
     /* intentionally empty */
 }
 
 double MneuMgM12gM3gX2Lookup::operator()(double * vars){
-    return vars[_array_id];
 }
 
 double c1(std::vector<double>::iterator m_it , double m){
