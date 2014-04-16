dd996b077a41b57dc2897d11f806bd9bf63110cb
Keesjan de Vries <keesjan777@gmail.com>
2014-04-16 10:20:06 +0100

    working version

--- sqlite_reduce_db.py
+++ sqlite_reduce_db.py
@@ -2,30 +2,23 @@
 # python modules
 import argparse
 import sqlite3
-from ctypes import cdll
 # private modules
-from py_modules.tools import *
+import py_modules.tools as tools
 import py_modules.CtypesWrappers as cw
 # user defined 
-import user.mc_old_setup
-import user.axes
-import user.spaces
-import user.vars_lookups
-import user.vars_functions
 import user.gauss_constraints
 import user.contour_constraints
-import user.constraints_sets
 import user.contours
-# shared library objects
-runlib=cdll.LoadLibrary('lib/libmylib.so')
+import user.constraints_sets
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--sqlite-db',help='sqlite database')
-    parser.add_argument('--outfile',help='output root file')
+    parser.add_argument('--db-in',help='sqlite database')
+    parser.add_argument('--db-out',help='sqlite database')
     parser.add_argument('--reference', default='chi2-chi2',
         help='Usually chi-functions, but in general the function that is mimimised to project the spaces')
-    parser.add_argument('--spaces', help='select plots from user/spaces.py')
+    parser.add_argument('--max-chi2',type=float,default=100)
+    parser.add_argument('--sql-where')
     return parser.parse_args()
 
 def get_array_ids_and_style(db):
@@ -41,43 +34,50 @@ def get_array_ids_and_style(db):
     conn.close()
     return array_ids_dict,'mcpp'
 
+def create_mcpp_observable_ids(db_in,db_out):
+    conn_in=sqlite3.connect(db_in)
+    conn_out=sqlite3.connect(db_out)
+    cur_in=conn_in.cursor()
+    cur_out=conn_out.cursor()
+    cur_out.execute('create table if not exists mcpp_observable_ids(points_column_name text, key1 text, key2 text)')
+    conn_out.commit()
+    cur_out.execute('select * from mcpp_observable_ids;')
+    if len(cur_out.fetchall())==0:
+        cur_in.execute('select * from mcpp_observable_ids;')
+        for row in cur_in.fetchall():
+            cur_out.execute('insert into mcpp_observable_ids values(?,?,?)',row)
+        conn_out.commit()
+    conn_in.close() 
+    conn_out.close() 
+
+
 if __name__ == '__main__':
+    #get arguments
     args=parse_args()
-    #FIXME: we need to only select the relevant columns and populate the various value functions accordingly
+    input_name=args.db_in
+    output_name=args.db_out
+    max_chi2=args.max_chi2
+    chi2_function_name=args.reference
+    # create mcpp_observable_ids in new database
+    create_mcpp_observable_ids(input_name,output_name)
     # get array ids
-    array_ids_dict, style=get_array_ids_and_style(args.sqlite_db)
-    # get spaces for which axis names are defined spaces
-    spaces=populate_spaces(user.axes.get(),user.spaces.get_spaces(args.spaces),args.reference)
+    array_ids_dict, style=get_array_ids_and_style(input_name)
     # populate vars_lookups, vars_functions, and gauss_constraints with array ids
-    vars_lookups={name: {'observable_ids': oids} for name, oids in user.vars_lookups.get().items()}
-    vars_lookups=populate_with_array_ids(vars_lookups,style,array_ids_dict)
-    vars_functions=populate_with_array_ids(user.vars_functions.get(),style,array_ids_dict)
-    gauss_constraints=populate_with_array_ids((user.gauss_constraints.get()),style,array_ids_dict)
-    contour_constraints=populate_with_array_ids((user.contour_constraints.get()),style,array_ids_dict)
+    gauss_constraints=tools.populate_with_array_ids((user.gauss_constraints.get()),style,array_ids_dict)
+    contour_constraints=tools.populate_with_array_ids((user.contour_constraints.get()),style,array_ids_dict)
     # populate contours and add to managers
-    contours=populate_contours(user.contours.get())
-    add_contours(contours)
-    # get axes that are in the spaces
-    axes_list=get_axes_list_from_spaces(spaces)
-    # for now the "valid" value functions are those for which array_ids are specified
-    valid_values_list=list(vars_lookups.keys())+list(vars_functions.keys())+ \
-        list(gauss_constraints.keys())+list(contour_constraints.keys())+\
-        [args.reference]
-    # populate the valid-and-required-by-spaces axes
-    axes=populate_axes(user.axes.get(),valid_values_list,axes_list)
+    contours=tools.populate_contours(user.contours.get())
+    tools.add_contours(contours)
     # now add values to the managers
-    add_vars_lookups(vars_lookups) 
-    add_vars_functions(vars_functions) 
-    add_gauss_constraints(gauss_constraints)
-    add_contour_constraints(contour_constraints)
+    tools.add_gauss_constraints(gauss_constraints)
+    tools.add_contour_constraints(contour_constraints)
     # look for chi2 calculators
     if args.reference in user.constraints_sets.constraints.keys():
-        add_chi2_calculator(args.reference,user.constraints_sets.get(args.reference))
-    # axes and spaces to managers
-    add_axes(axes)
-    pp(spaces)
-    add_spaces(spaces)
-    # input and output files
-    outfile=args.outfile
+        tools.add_chi2_calculator(args.reference,user.constraints_sets.get(args.reference))
+    # make selection query
+    sql_where=''
+    if args.sql_where is not None:
+        sql_where='where ' + args.sql_where
+    select_query='select rowid, * from points {};'.format(sql_where)
     #finally make the plots
-    cw.sqlite_make_plots(args.sqlite_db,'select rowid, * from points;',outfile)
+    cw.sqlite_reduce_db(input_name,output_name,select_query,chi2_function_name,max_chi2)
